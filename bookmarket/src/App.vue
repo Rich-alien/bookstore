ec9e1d7b70e36e46716bbb5b42823b5a27750945
Rich <richalien@yandex.ru>
2020-04-04 04:34:44 +0400

    первичные настройки

--- bookmarket/src/App.vue
+++ bookmarket/src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
@@ -18,8 +16,7 @@
 }
 
 #nav {
-  padding: 30px;
-
+  padding: 0px;
   a {
     font-weight: bold;
     color: #2c3e50;
---
+++ bookmarket/src/components/v-top__header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="topHeader">
+    <ul class="queue">
+      <li class="queue__link">{{l1}}</li>
+      <li class="queue__link">{{l2}}</li>
+      <li class="queue__link">{{l3}}</li>
+      <li class="queue__link">{{l4}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      l1: 'Sign in',
+      l2: 'My Account',
+      l3: 'Order Status',
+      l4: 'Help'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  queue{
+		
+	}
+</style>
